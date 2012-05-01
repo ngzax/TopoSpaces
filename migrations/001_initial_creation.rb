e9aaa647ed33bad5b4d282d758dcf0303b48da68
N. G. Zax <ngzax@comcast.net>
2012-04-30 20:39:31 -0400

    Still fixing the creation script.

--- migrations/001_initial_creation.rb
+++ migrations/001_initial_creation.rb
@@ -8,14 +8,14 @@ Sequel.migration do
 
     create_table(:forums) do
       primary_key :id
-      String   :name, :null => false, :primary_key => true, :size => 8
+      String   :name, :null => false, :size => 8
       DateTime :dt, :null => false
       foreign_key :community_id, :communities, :null => false
     end
 
     create_table(:thoughts) do
       primary_key :id
-      String   :name, :null => false, :primary_key => true, :size => 8
+      String   :name, :null => false, :size => 8
       DateTime :dt,   :null => false
       String   :body, :null => false, :text => true
       foreign_key :forum_id, :forums,  :null => false
